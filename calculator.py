1e2226720db17247fa71dd5fe37c96a4095403d7
ashmrz <ashkan.m10@gmail.com>
2021-03-08 10:41:03 +0330

    Add subtract operator method to calculator class

--- calculator.py
+++ calculator.py
@@ -11,8 +11,8 @@ class Calculator:
             The name of this instance of the calculator.
         """
         self.name = name
-		
-	def compute_add(self, op1, op2):
+
+    def compute_add(self, op1, op2):
         """
         This function return sum of its inputs
 
@@ -25,3 +25,16 @@ class Calculator:
             return the sum of two operands
         """
         return op1 + op2
+
+    def compute_sub(self, op1, op2):
+        """
+        The computer method for subtract operation.
+
+        :param op1: float
+            The first operand
+        :param op2: float
+            The second operand
+        :return : float
+            return the subtraction of two operands
+        """
+        return op1 - op2
--- tests.py
+++ tests.py
@@ -13,9 +13,15 @@ class TestCalculator(unittest.TestCase):
         calculator = Calculator(name)
         self.assertEqual(calculator.name, name)
 
-	def test_add(self):
+    def test_add(self):
         name = 'Calc'
         calculator = Calculator(name)
         self.assertEqual(calculator.compute_add(10, 20), 30)
         self.assertAlmostEqual(calculator.compute_add(-5, 0.2), -4.8)
         self.assertAlmostEqual(calculator.compute_add(10, -8.7), 1.3)
+
+    def test_sub(self):
+        name = 'Calc'
+        calculator = Calculator(name)
+        self.assertEqual(calculator.compute_sub(2, 10), -8)
+        self.assertAlmostEqual(calculator.compute_sub(-1, 0.5), -1.5)
