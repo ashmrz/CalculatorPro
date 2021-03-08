f9983c41ab7889351e385311faf613cd7c0ab075
AradM <arad.mohammadi99@gmail.com>
2021-03-08 10:37:26 +0330

    Add sum function with its tests

--- calculator.py
+++ calculator.py
@@ -11,3 +11,17 @@ class Calculator:
             The name of this instance of the calculator.
         """
         self.name = name
+		
+	def compute_add(self, op1, op2):
+        """
+        This function return sum of its inputs
+
+        :param op1: float
+            The first operand
+        :param op2: float
+            The second operand
+
+        :return : float
+            return the sum of two operands
+        """
+        return op1 + op2
--- tests.py
+++ tests.py
@@ -12,3 +12,10 @@ class TestCalculator(unittest.TestCase):
         name = 'Ashkan'
         calculator = Calculator(name)
         self.assertEqual(calculator.name, name)
+
+	def test_add(self):
+        name = 'Calc'
+        calculator = Calculator(name)
+        self.assertEqual(calculator.compute_add(10, 20), 30)
+        self.assertAlmostEqual(calculator.compute_add(-5, 0.2), -4.8)
+        self.assertAlmostEqual(calculator.compute_add(10, -8.7), 1.3)
